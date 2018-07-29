5b6fcbd33058577303b54371612a6d72bf129a4a
Emile Fokkema <emilefokkema@gmail.com>
2018-07-29 19:28:59 +0200

    override the simple handler, not the base one

--- serve.py
+++ serve.py
@@ -3,8 +3,25 @@ import socketserver
 
 PORT = 8000
 
-Handler = http.server.SimpleHTTPRequestHandler
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+	def __init__(self, request, client_address, server):
+		super(MyHandler, self).__init__(request, client_address, server)
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+	def do_GET():
+		if self.path.startswith('/api'):
+			self.do_api_GET()
+		else:
+			super(MyHandler, self).do_GET()
+
+	def do_api_GET(self):
+		self.send_response(200)
+		self.send_header('Content-type','text/html')
+		self.end_headers()
+		self.wfile.write("ada") #Doesnt work
+		return
+
+#Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
     print("serving at port", PORT)
     httpd.serve_forever()
