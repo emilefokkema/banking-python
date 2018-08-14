241221958d595ed40e04f30ed04c060c8c267da9
Emile Fokkema <emilefokkema@gmail.com>
2018-08-14 11:04:44 +0200

    simplify json printers

--- jsonprinter.py
+++ jsonprinter.py
@@ -2,21 +2,30 @@ from datetime import date
 import json
 import random
 class JsonPrinter(object):
-	def __init__(self):
+	def __init__(self, doexit = None):
 		self.object = {}
 		self.hasExited = False
+		if doexit == None:
+			doexit = lambda obj,written:0
+		self.doexit = doexit
+		self.written = False
 
 	def getObj(self):
 		return self.object
 
 	def indent(self, key):
-		return JsonPropertyPrinter(self, key)
+		return JsonPrinter(lambda obj,written:self.writeLine(key,obj) if written else 0)
 
 	def indentList(self, key):
-		return JsonListPrinter(self, key)
+		return JsonListPrinter(lambda obj,written:self.writeLine(key,obj))
+
+	def writeToFile(self, obj, fileName):
+		print('saving json: '+fileName)
+		with open(fileName+'.json', 'w') as jsonFile:
+			json.dump(obj, jsonFile)
 
 	def startFile(self, fileName):
-		return JsonFilePrinter(fileName)
+		return JsonPrinter(lambda obj,written:self.writeToFile(obj, fileName))
 
 	def sanitizeValue(self, value):
 		if isinstance(value, date):
@@ -25,9 +34,7 @@ class JsonPrinter(object):
 
 	def writeLine(self, key, value):
 		self.object[key] = self.sanitizeValue(value)
-
-	def doexit(self):
-		pass
+		self.written = True
 
 	def __enter__(self):
 		return self
@@ -36,65 +43,22 @@ class JsonPrinter(object):
 		if self.hasExited:
 			return
 		self.hasExited = True
-		self.doexit()
+		self.doexit(self.object, self.written)
 
-class JsonFilePrinter(JsonPrinter):
-	def __init__(self, fileName):
-		super(JsonFilePrinter, self).__init__()
-		self.fileName = fileName
-
-	def doexit(self):
-		print('saving json: '+self.fileName)
-		with open(self.fileName+'.json', 'w') as jsonFile:
-			json.dump(self.object, jsonFile)
-
-class CompositeJsonPrinter(JsonPrinter):
-	def __init__(self, printer):
-		super(CompositeJsonPrinter, self).__init__()
-		self.printer = printer
-		self.lineWritten = False
-
-	def printToParent(self):
-		pass
-
-	def writeLine(self, key, value):
-		super(CompositeJsonPrinter, self).writeLine(key, value)
-		self.lineWritten = True
-
-	def doexit(self):
-		if self.lineWritten:
-			self.printToParent()
-
-class JsonPropertyPrinter(CompositeJsonPrinter):
-	def __init__(self, printer, key):
-		super(JsonPropertyPrinter, self).__init__(printer)
-		self.key = key
-
-	def printToParent(self):
-		self.printer.writeLine(self.key, self.object)
-
-class JsonListPrinter(JsonPropertyPrinter):
-	def __init__(self, printer, key):
-		super(JsonListPrinter, self).__init__(printer, key)
+class JsonListPrinter(JsonPrinter):
+	def __init__(self, doexit = None):
+		super(JsonListPrinter, self).__init__(doexit)
 		self.object = []
 
 	def addValue(self, obj):
 		self.object.append(self.sanitizeValue(obj))
 
 	def indentItem(self):
-		return JsonListItemPrinter(self)
+		return JsonPrinter(lambda obj,written:self.addValue(obj) if written else 0)
 
 	def writeLine(self, key, value):
 		raise Exception('list writer cannot')
 
 	def indent(self, key):
 		raise Exception('list writer cannot')
-
-	def doexit(self):
-		self.printToParent()
-
-class JsonListItemPrinter(CompositeJsonPrinter):
-
-	def printToParent(self):
-		self.printer.addValue(self.object)
 
