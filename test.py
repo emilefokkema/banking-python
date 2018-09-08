2f55c8f80ea4769744dfdd9a9518a140d66e882d
Emile Fokkema <emilefokkema@gmail.com>
2018-09-08 11:50:09 +0200

    improve assertions and add a row collection test

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from rowfactory import RowFactory
 from rowcheckerfactory import RowCheckerFactory
+from rowcollection import RowCollectionFactory
+from jsonprinter import JsonPrinter
 from datetime import datetime
 from direction import Direction
 import traceback
@@ -10,16 +12,57 @@ def test(testClass):
 	testClasses.append(testClass)
 	return testClass
 
+def getJsonObj(printable):
+	printer = JsonPrinter()
+	printable.printSelf(printer)
+	return printer.getObj()
+
 class TestFailure(BaseException):
 	pass
 
+def wrapAssertion(whatToPrint):
+	def wrapper(asserter):
+		def wrapFn(*args):
+			try:
+				asserter(*args)
+			except AssertionError:
+				print(whatToPrint(*args))
+				raise
+		return wrapFn
+	return wrapper
+
+@wrapAssertion(lambda one, two:'expected '+repr(one)+' to equal '+repr(two))
 def assertEquals(one, two):
-	try:
-		assert one == two
-	except AssertionError:
-		print('failure: expected '+repr(one)+' to equal '+repr(two))
-		traceback.print_exc()
-		raise TestFailure()
+	assert one == two
+
+@wrapAssertion(lambda obj, _type:'expected '+repr(obj)+' to be of type '+repr(_type))
+def assertInstance(obj, _type):
+	assert isinstance(obj, _type)
+
+@wrapAssertion(lambda key, _dict:'expected dictionary '+repr(_dict)+' to contain key '+key)
+def assertInDict(key, _dict):
+	assert key in _dict
+
+@wrapAssertion(lambda list1, list2:'expected list '+repr(list1)+' and list '+repr(list2)+' to be of equal length')
+def assertEqualLength(list1, list2):
+	assert len(list1) == len(list2)
+
+def assertDeepEquals(obj1, obj2):
+	if isinstance(obj1, dict):
+		assertInstance(obj2, dict)
+		for p in obj1:
+			assertInDict(p, obj2)
+			assertDeepEquals(obj1[p], obj2[p])
+		for p in obj2:
+			assertInDict(p, obj1)
+			assertDeepEquals(obj2[p], obj1[p])
+	elif isinstance(obj1, list):
+		assertInstance(obj2, list)
+		assertEqualLength(obj1, obj2)
+		for index in range(len(obj1)):
+			assertDeepEquals(obj1[index], obj2[index])
+	else:
+		assertEquals(obj1, obj2)
 
 class RowFactoryTest:
 	def __init__(self):
@@ -140,6 +183,64 @@ class IncomingTest(RowCheckerFactoryTest):
 		row = self.rowFactory.createRow(['20180509','34.34','in','something'])
 		assertEquals(rowChecker.checkRow(row), True)
 
+class RowCollectionTest:
+	def __init__(self):
+		self.rowFactory = RowFactory({
+			'date':{
+				'columnIndex':0,
+				'pattern':'yyyymmdd'
+			},
+			'amount':{
+				'columnIndex':1
+			},
+			'direction':{
+				'columnIndex':2,
+				'incoming':'in',
+				'outgoing':'out'
+			},
+			'additional':[
+				{
+					'name':'info',
+					'columnIndex':3
+				}
+			]
+		})
+		self.rowCollectionFactory = RowCollectionFactory(self.rowFactory)
+
+@test
+class RowCollectionTestWithStringConversion(RowCollectionTest):
+
+	def test(self):
+		collection = self.rowCollectionFactory.getDefault({
+			'properties':[
+				{
+					'name':'date',
+					'source':'date'
+				},
+				{
+					'name':'infoPart',
+					'source':'info',
+					'conversion':{
+						'type':'string',
+						'match':r'(?<=abraham\s)(?:(?!\W).)*'
+					}
+				},
+				{
+					'name':'amount',
+					'source':'amount'
+				}
+			]
+		})
+		collection.addRow(self.rowFactory.createRow(['20180509','34.67', 'in', 'abraham lincoln\'s car']))
+		collectionObj = getJsonObj(collection)
+		firstItem = collectionObj['items'][0]
+		assertDeepEquals(firstItem,[
+			{'name':'date','type':'date','value':'09-05-2018 00:00'},
+			{'name':'infoPart','type':'string','value':'lincoln'},
+			{'name':'amount','type':'amount','value':3467}
+		])
+
+
 def runTests():
 	failed = 0
 	passed = 0
@@ -149,8 +250,6 @@ def runTests():
 		try:
 			test.test()
 			passed += 1
-		except TestFailure:
-			failed += 1
 		except BaseException:
 			traceback.print_exc()
 			failed += 1
