70cd554f16048e7061cd830bd43af652c85a6a1a
Emile Fokkema <emilefokkema@gmail.com>
2018-09-19 20:21:06 +0200

    make sure the local data provider creates a directory if it doesn't exist

--- src/dataprovider.py
+++ src/dataprovider.py
@@ -2,8 +2,11 @@ import json
 import os
 
 class DataProvider:
+	def __init__(self):
+		self.dirname = 'userdata'
+
 	def getPathToKey(self, key):
-		return 'userdata/'+key+'.json'
+		return self.dirname+'/'+key+'.json'
 
 	def getItem(self, key):
 		path = self.getPathToKey(key)
@@ -13,6 +16,9 @@ class DataProvider:
 		return None
 
 	def setItem(self, key, item):
+		exists = os.path.isdir(self.dirname)
+		if not exists:
+			os.mkdir(self.dirname)
 		with open(self.getPathToKey(key),'w') as file:
 			json.dump(item, file)
 
