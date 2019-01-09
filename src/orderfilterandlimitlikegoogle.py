fe4249029c3c60c77db3464ccbd92dd43c23b526
Emile Fokkema <emilefokkema@gmail.com>
2019-01-09 13:34:13 +0100

    make test pass

--- src/orderfilterandlimitlikegoogle.py
+++ src/orderfilterandlimitlikegoogle.py
@@ -3,12 +3,6 @@ from src.orderedlikegoogle import orderedLikeGoogle
 
 def orderFilterAndLimitLikeGoogle(iterable, order=(), filters=(), limit=None):
 	ordered = orderedLikeGoogle(iterable, order=order)
-	result = []
-	limit = limit or 0
-	resultcount = 0
-	for item in ordered:
-		resultcount += 1
-		if filterItemLikeGoogle(item, filters) and (limit == 0 or resultcount <= limit):
-			result.append(item)
-	
-	return result
+	filtered = (item for item in ordered if filterItemLikeGoogle(item, filters))
+	limited = (item for index,item in enumerate(filtered) if not limit or index < limit)
+	return [item for item in limited]
